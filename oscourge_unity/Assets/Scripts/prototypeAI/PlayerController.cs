ca2276fcc6b455350d0e91db2288801e8e5c3be2
dodolebg <49374440+dodolebg@users.noreply.github.com>
2019-05-16 20:40:28 +0200

    Menu + AI poursuite

---
+++ oscourge_unity/Assets/Scripts/prototypeAI/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    private Rigidbody2D myRigidbody;
+
+    public float jumpSpeed;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+
+    public bool isGrounded; 
+
+    // Start is called before the first frame update
+    void Start() {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        if (Input.GetAxisRaw("Horizontal") > 0f) {
+            myRigidbody.velocity = new Vector3(moveSpeed, myRigidbody.velocity.y, 0f);
+            transform.localScale = new Vector3(2f, 2f, 1f);
+        } else if (Input.GetAxisRaw("Horizontal") < 0f) {
+            myRigidbody.velocity = new Vector3(-moveSpeed, myRigidbody.velocity.y, 0f);
+            transform.localScale = new Vector3(-2f, 2f, 1f);
+        } else {
+            myRigidbody.velocity = new Vector3(0f, myRigidbody.velocity.y, 0f);
+        }
+
+        if (Input.GetButtonDown("Jump") ) {
+            myRigidbody.velocity = new Vector3(myRigidbody.velocity.x, jumpSpeed, 0f);
+        }
+    }
+
+   }
---
+++ oscourge_unity/Assets/Scripts/prototypeAI/mechant.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mechant : MonoBehaviour
+{
+
+  public GameObject player;
+  public float moveSpeed;
+  private Rigidbody2D myRigidbody;
+
+  public float jumpSpeed;
+
+  private bool seen;
+  private int z;
+
+    // Start is called before the first frame update
+  void Start() {
+    myRigidbody = GetComponent<Rigidbody2D>();
+    seen=false;
+    z=0;
+}
+
+void Update()
+{
+    float i=player.transform.position.x-transform.position.x;
+    if((i>0f&&i<4f)||(i<0f && i>-4f) ){
+        seen=true;
+    }
+
+    if(seen){
+    	if(player.transform.position.x>transform.position.x){
+            myRigidbody.velocity = new Vector3(moveSpeed, myRigidbody.velocity.y, 0f);
+            transform.localScale = new Vector3(2f, 2f, 1f);
+        }
+        else if(player.transform.position.x<transform.position.x){
+
+            myRigidbody.velocity = new Vector3(-moveSpeed, myRigidbody.velocity.y, 0f);
+            transform.localScale = new Vector3(-2f, 2f, 1f);
+        }
+
+         if(player.transform.position.y>transform.position.y+3){
+         myRigidbody.velocity = new Vector3(myRigidbody.velocity.x, jumpSpeed, 0f);
+     }
+ } else{
+    if(z<30){
+     myRigidbody.velocity = new Vector3(moveSpeed, myRigidbody.velocity.y, 0f);
+     transform.localScale = new Vector3(2f, 2f, 1f);
+     z++;
+     Debug.Log("Droite");
+ }
+ else if (z<60){
+
+    myRigidbody.velocity = new Vector3(-moveSpeed, myRigidbody.velocity.y, 0f);
+    transform.localScale = new Vector3(-2f, 2f, 1f);
+     Debug.Log("Gauche");
+     z++;
+}
+else if (z==60){
+    z=0;
+}
+
+
+}
+
+
+}
+void OnCollisionEnter2D (Collision2D col)
+{
+    if(col.gameObject.name == "gentil")
+    {
+       col.transform.position=new Vector3(-7f,-1.5f,0);
+       seen=false;
+    }
+}
+}
