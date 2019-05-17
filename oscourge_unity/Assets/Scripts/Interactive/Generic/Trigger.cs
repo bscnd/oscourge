034498be0a85de0686247491cfa035709a32b296
Sc1pi0 <30751820+Sc1pi0@users.noreply.github.com>
2019-05-17 12:51:31 +0200

    Adding lever sprites, animation and script

---
+++ oscourge_unity/Assets/Scripts/Interactive/Generic/Trigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+	private bool active = false;
+
+	public void activate(){
+		this.active = true;
+	}
+
+	public void deactivate(){
+		this.active = false;
+	}
+
+	public bool getState(){
+		return this.active;
+	}
+}
