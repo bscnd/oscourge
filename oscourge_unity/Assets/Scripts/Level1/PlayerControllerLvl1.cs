0283dd0c548869cd2fa5d1d7cdad470f935d02b0
Sc1pi0 <30751820+Sc1pi0@users.noreply.github.com>
2019-05-17 17:46:37 +0200

    Modification off the player controller to handle levers

--- oscourge_unity/Assets/Scripts/Level1/PlayerControllerLvl1.cs
+++ oscourge_unity/Assets/Scripts/Level1/PlayerControllerLvl1.cs
@@ -2,40 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerControllerLvl1 : MonoBehaviour {
 
-    public float moveSpeed;
-    private Rigidbody2D myRigidbody;
+	public float moveSpeed;
+	private Rigidbody2D myRigidbody;
 
-    public float jumpSpeed;
+	public float jumpSpeed;
 
-    public Transform groundCheck;
-    public float groundCheckRadius;
-    public LayerMask whatIsGround;
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
 
-    public bool isGrounded; 
+	public bool isGrounded; 
 
-    // Start is called before the first frame update
-    void Start() {
-        myRigidbody = GetComponent<Rigidbody2D>();
-    }
+	// Start is called before the first frame update
+	void Start() {
+		myRigidbody = GetComponent<Rigidbody2D>();
+	}
 
-    // Update is called once per frame
-    void Update() {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+	private void updateInteractiveObjects(){
+		GameObject[] interactiveObjects = GameObject.FindGameObjectsWithTag("Interactive");
+		Lever nearestLever = null;
+		float distanceLever = float.PositiveInfinity;
+		float maximumDistance = 1.2F;
 
-        if (Input.GetAxisRaw("Horizontal") > 0f) {
-            myRigidbody.velocity = new Vector3(moveSpeed, myRigidbody.velocity.y, 0f);
-            transform.localScale = new Vector3(2f, 2f, 1f);
-        } else if (Input.GetAxisRaw("Horizontal") < 0f) {
-            myRigidbody.velocity = new Vector3(-moveSpeed, myRigidbody.velocity.y, 0f);
-            transform.localScale = new Vector3(-2f, 2f, 1f);
-        } else {
-            myRigidbody.velocity = new Vector3(0f, myRigidbody.velocity.y, 0f);
-        }
+		foreach(GameObject obj in interactiveObjects){
+			float temp = Vector2.Distance(this.transform.position, obj.transform.position);
+			if(Input.GetButtonDown("ContextualAction")){
+				Lever aLever = obj.GetComponent(typeof(Lever)) as Lever;
+				if(aLever != null){
+					if(temp < distanceLever && temp < maximumDistance){
+						distanceLever = temp;
+						nearestLever = aLever;
+					}
+				}
 
-        if (Input.GetButtonDown("Jump") && isGrounded) {
-            myRigidbody.velocity = new Vector3(myRigidbody.velocity.x, jumpSpeed, 0f);
-        }
-    }
+			}
+		}
+
+		if(nearestLever != null){
+			nearestLever.trigger();
+		}
+	}
+
+	// Update is called once per frame
+	void Update() {
+		isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+		if (Input.GetAxisRaw("Horizontal") > 0f) {
+			myRigidbody.velocity = new Vector3(moveSpeed, myRigidbody.velocity.y, 0f);
+			transform.localScale = new Vector3(2f, 2f, 1f);
+		} else if (Input.GetAxisRaw("Horizontal") < 0f) {
+			myRigidbody.velocity = new Vector3(-moveSpeed, myRigidbody.velocity.y, 0f);
+			transform.localScale = new Vector3(-2f, 2f, 1f);
+		} else {
+			myRigidbody.velocity = new Vector3(0f, myRigidbody.velocity.y, 0f);
+		}
+
+		if (Input.GetButtonDown("Jump") && isGrounded) {
+			myRigidbody.velocity = new Vector3(myRigidbody.velocity.x, jumpSpeed, 0f);
+		}
+
+		updateInteractiveObjects();
+	}
 }
