82b833eff7c944ccb232085d7616d0fb87deddec
Leocvn <leo.cauvin50@gmail.com>
2019-05-17 11:46:46 +0200

    Add sprites & rename script PlayerControllerLvl1
    bougie_anim, sablier, tonneau_couche, tonneau_debout, coffre

--- oscourge_unity/Assets/Scripts/prototypeAI/PlayerController.cs
+++ oscourge_unity/Assets/Scripts/prototypeAI/PlayerController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerControllerLvl1 : MonoBehaviour {
 
     public float moveSpeed;
     private Rigidbody2D myRigidbody;
