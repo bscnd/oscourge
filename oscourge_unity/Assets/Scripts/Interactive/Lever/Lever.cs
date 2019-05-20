a0e4fa494c91f5444466f56391a24a9135d7b29e
Sc1pi0 <30751820+Sc1pi0@users.noreply.github.com>
2019-05-20 23:00:28 +0200

    Add a prefab for the pressure plate, the lever and the chain

--- oscourge_unity/Assets/Scripts/Interactive/Lever/Lever.cs
+++ oscourge_unity/Assets/Scripts/Interactive/Lever/Lever.cs
@@ -3,61 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Lever : MonoBehaviour
-
 {
-	Animator anim;
-	public Trigger[] triggers;
-
-	int hashActivation = Animator.StringToHash("lever_activation");
-	int hashDeactivation = Animator.StringToHash("lever_deactivation");
-
-	private enum AnimEvent { Activate, Deactivate, NoEvent };
-	private enum State { active, inactive };
-	private AnimEvent animEvent;
-	private State state;
+	private Animator myAnim;
+	public Chain[] chains;
+	
+	int hashIsTriggered = Animator.StringToHash("isTriggered");
 
 	// Start is called before the first frame update
 	void Start()
 	{
-		anim = GetComponent<Animator>();
-		animEvent = AnimEvent.NoEvent;
-		state = State.inactive;
+		myAnim = GetComponent<Animator>();
 	}
 
-	// Update is called once per frame
-	void Update()
-	{
-		if(this.animEvent == AnimEvent.Activate){
-			anim.Play(hashActivation);
-			this.animEvent = AnimEvent.NoEvent;
-			this.state = State.active;
-
-			foreach(Trigger trigger in triggers){
-				trigger.activate();
-			}
-		}
-
-		else if(this.animEvent == AnimEvent.Deactivate){
-			anim.Play(hashDeactivation);
-			this.animEvent = AnimEvent.NoEvent;
-			this.state = State.inactive;
+	public void updateInteractiveObjects(){
+		foreach(Chain chain in chains){
+			chain.trigger(myAnim.GetBool(hashIsTriggered), this.GetInstanceID());
 		}
 	}
 
-	public void trigger(){
-		if(this.state == State.inactive){
-			this.activate();
+	void Update(){
+		if(Input.GetButtonDown("ContextualAction")){
+			myAnim.SetBool(hashIsTriggered, !myAnim.GetBool(hashIsTriggered));
+			updateInteractiveObjects();
 		}
-		else{
-			this.deactivate();
-		}
-	}
-
-	private void activate(){
-		this.animEvent = AnimEvent.Activate;
-	}
-
-	private void deactivate(){
-		this.animEvent = AnimEvent.Deactivate;
 	}
 }
