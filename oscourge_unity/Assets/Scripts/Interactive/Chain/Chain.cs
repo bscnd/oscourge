e5b1f17379e6f60c3f580b9c8c2973d30078eca0
Sc1pi0 <30751820+Sc1pi0@users.noreply.github.com>
2019-05-21 20:05:54 +0200

    Adding visual indicator for the interactive objects, to show what they trigger

--- oscourge_unity/Assets/Scripts/Interactive/Chain/Chain.cs
+++ oscourge_unity/Assets/Scripts/Interactive/Chain/Chain.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class Chain : MonoBehaviour
 {
+	// TODO : Put the color code in the script indicators
+	private static Color[] colors =
+	{
+		new Color(0F, 0F, 0F) // Black
+		,new Color(255F, 72F, 226F) // Pink
+		,new Color(13F, 113F, 148F) // Light blue
+		,new Color(32F, 123F, 32F) // Light green
+		,new Color(247F, 4F, 4F) // Red
+		,new Color(242F, 255F, 0F) // Yellow
+		,new Color(123F, 123F, 108F) // Grey
+		,new Color(36F, 0F, 173F) // True blue
+		,new Color(173F, 0F, 91F) // Magenta
+		,new Color(115F, 64F, 40F) // Brown
+	};
+
+	private static int colorIndex = 0;
+
 	private Animator myAnim;
 
 	// The number of pressurePlates/levers/... that has to be activated to trigger this gameobject
@@ -21,6 +38,7 @@ public class Chain : MonoBehaviour
 		myAnim = GetComponent<Animator>();
 		activators = new SortedSet<int>();
 		chainCollider = GetComponent<Collider2D>();
+
 	}
 
 	public void trigger(bool isActivate, int objectId){
@@ -40,5 +58,9 @@ public class Chain : MonoBehaviour
 	
 	public void enableCollider(){
 		this.chainCollider.enabled = true;
+	}
+
+	public static Color getAColor(){
+		return colors[colorIndex++];
 	}
 }
--- oscourge_unity/Assets/Scripts/Interactive/Lever/Lever.cs
+++ oscourge_unity/Assets/Scripts/Interactive/Lever/Lever.cs
@@ -7,13 +7,17 @@ public class Lever : MonoBehaviour
 	private Animator myAnim;
 	public Chain[] chains;
 	private bool playerIsNear = false;
+	public GameObject triggerIndicator;
+	//private Transfrom transform;
 	
 	int hashIsTriggered = Animator.StringToHash("isTriggered");
+	
 
 	// Start is called before the first frame update
 	void Start()
 	{
 		myAnim = GetComponent<Animator>();
+		initIndicators();
 	}
 
 	public void updateInteractiveObjects(){
@@ -38,6 +42,31 @@ public class Lever : MonoBehaviour
 	private void OnTriggerEnter2D(Collider2D collision) {
 		if(collision.tag == "Player") {
 			playerIsNear = true;
+		}
+	}
+
+	private void initIndicators(){
+		if(triggerIndicator != null && triggerIndicator.tag == "Indicator"){
+			if(chains.Length > 0){
+				float[] angle = new float[5];
+				angle[0] = 90;
+				angle[1] = 120;
+				angle[2] = 60;
+				angle[3] = 150;
+				angle[4] = 30;
+
+				int index = 0;
+				foreach(Chain chain in chains){
+					Vector3 position = new Vector3(Mathf.Cos(angle[index] * Mathf.Deg2Rad), Mathf.Sin(angle[index] * Mathf.Deg2Rad), 0);
+					position += this.transform.position;
+					GameObject indicator = Instantiate(triggerIndicator, position, Quaternion.identity);
+					SpriteRenderer renderer = indicator.GetComponent<SpriteRenderer>();
+					Material mat = new Material(Shader.Find("Sprites/Diffuse"));
+					mat.color = Chain.getAColor();
+					renderer.material = mat;
+					index++;
+				}
+			}
 		}
 	}
 }
