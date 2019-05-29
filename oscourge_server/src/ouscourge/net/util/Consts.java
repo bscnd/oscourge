b8031d7da3c0506aba6941f5d6bf6e5ef1bf7d6f
Nils <nils.richard@free.fr>
2019-05-29 11:53:43 +0200

    Adding const for strings

---
+++ oscourge_server/src/ouscourge/net/util/Consts.java
@@ -0,0 +1,10 @@
+package ouscourge.net.util;
+
+public class Consts {
+	public static final String gameFullMsg = "this game is full";
+	public static final String tooManyClients = "too many clients";
+	public static final String wrongMsgType = "wrong message type";
+	public static final String badIndex = "Bad index";
+	public static final String newClient = "New client connected : ";
+
+}
