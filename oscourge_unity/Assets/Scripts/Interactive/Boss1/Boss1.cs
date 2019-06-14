654025537d564158dd94cee7592db1227d140a01
Sc1pi0 <30751820+Sc1pi0@users.noreply.github.com>
2019-06-14 11:57:18 +0200

    Boss1 example added with fixed platforms (Player doesn't slide on them anymore) and new boss control scripts

---
+++ oscourge_unity/Assets/Scripts/Interactive/Boss1/Boss1.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss1 : MonoBehaviour
+{
+	// TODO Move update boss control here
+
+	private Boss1Control[] boss;
+	public Chain chain;
+
+	// The number of pressure plate activation by the two players needed to trigger the chain
+	private int activationNeeded = 3;
+	private int activationNumber = 0;
+
+	void Start()
+	{
+		boss = new Boss1Control[2];
+
+		if(this.gameObject.transform.childCount == 2){
+			GameObject control0 = this.transform.GetChild(0).gameObject;
+			GameObject control1 = this.transform.GetChild(1).gameObject;
+			boss[0] = control0.GetComponent<Boss1Control>();
+			boss[1] = control1.GetComponent<Boss1Control>();
+
+		}
+		else{
+			Debug.Log("This object must have 2 childs with Boss1Control scripts attached to it !");
+			this.enabled = false;
+		}
+
+		if(boss[0] == null || boss[1] == null){
+			Debug.Log("One or both Boss1Control scripts is null");
+			this.enabled = false;
+		}
+
+		if(chain == null){
+			Debug.Log("No chain is attached !");
+			this.enabled = false;
+		}
+	}
+
+	void Update()
+	{
+		if(boss[0].isActivated() && boss[1].isActivated()){
+			activationNumber++;
+			boss[0].switchPlatform();
+			boss[1].switchPlatform();
+		}
+		
+		if(activationNumber >= activationNeeded){
+			chain.trigger(true, this.GetInstanceID());
+		}
+	}
+}
---
+++ oscourge_unity/Assets/Scripts/Interactive/Platform/MovingPlatform.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MovingPlatform : MonoBehaviour
+{
+	private Collider2D platformCollider;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		platformCollider = GetComponent<Collider2D>();
+
+		if(platformCollider == null){
+			Debug.Log("A Collision2D object must be attached to this script to make it work !");
+			this.enabled = false;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+		collision.collider.transform.SetParent(this.transform);
+	}
+
+	void OnCollisionExit2D(Collision2D collision){
+		collision.collider.transform.SetParent(null);
+	}
+}
