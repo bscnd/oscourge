eb09db1c599af682a89c3cd2241e7ce80ac4b7d1
dodolebg <49374440+dodolebg@users.noreply.github.com>
2019-06-17 15:07:38 +0200

    Adding Audio Mixer, new SFX and Post Processing
    very nice

---
+++ oscourge_unity/Assets/Scripts/Menu/sliderScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class sliderScript : MonoBehaviour {
+
+    public AudioMixer mixer;
+    public string name;
+    public Slider slider;
+
+        void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat(name, 0.75f);
+    }
+
+    public void SetLevel (float sliderValue)
+    {
+        mixer.SetFloat(name, Mathf.Log10(sliderValue) * 20);
+          PlayerPrefs.SetFloat(name, sliderValue);
+    }
+}
--- oscourge_unity/Assets/Scripts/SFX/SFX.cs
+++ oscourge_unity/Assets/Scripts/SFX/SFX.cs
@@ -5,44 +5,65 @@ using UnityEngine;
 public class SFX : MonoBehaviour
 {
 
-public AudioSource jump1,jump2;
-public AudioSource run;
-public AudioSource explosion;
-public AudioSource hurt;
-public AudioSource teleport;
+	public AudioSource jump1,jump2;
+	public AudioSource run;
+	public AudioSource explosion;
+	public AudioSource hurt;
+	public AudioSource teleport;
+	public AudioSource button;
+	public AudioSource music;
+	public AudioSource menuMusic;
 
 
 
-public void JumpSound1(){
-	jump1.Play();
-}
+	public void JumpSound1(){
+		jump1.Play();
+	}
 
-public void JumpSound2(){
-	jump2.Play();
-}
+	public void JumpSound2(){
+		jump2.Play();
+	}
 
 
-public void RunSound(){
-	if(!run.isPlaying){
-	run.Play();
-}
-}
+	public void RunSound(){
+		if(!run.isPlaying){
+			run.Play();
+		}
+	}
 
-public void RunStop(){
-	run.Stop();
-}
+	public void RunStop(){
+		run.Stop();
+	}
 
-public void ExplosionSound(){
-	explosion.Play();
-}
+	public void ExplosionSound(){
+		explosion.Play();
+	}
 
-public void HurtSound(){
-	hurt.Play();
-}
+	public void HurtSound(){
+		hurt.Play();
+	}
 
-public void TeleportSound(){
-	teleport.Play();
-}
+	public void TeleportSound(){
+		teleport.Play();
+	}
 
- 
+	public void ButtonSound(){
+		button.Play();
+	}
+
+	public void Music(){
+		music.Play();
+	}
+
+	public void MusicStop(){
+		music.Stop();
+	}
+
+	public void MenuMusic(){
+		menuMusic.Play();
+	}
+
+	public void MenuMusicStop(){
+		menuMusic.Stop();
+	}
 }
