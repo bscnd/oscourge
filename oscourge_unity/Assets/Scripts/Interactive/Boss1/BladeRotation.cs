5b5095db4cf1f55009403c53911c7cd38eb12aed
Sc1pi0 <30751820+Sc1pi0@users.noreply.github.com>
2019-06-18 17:42:04 +0200

    Adding windmill and blades in Boss1 prefab with a script for the blade rotation

---
+++ oscourge_unity/Assets/Scripts/Interactive/Boss1/BladeRotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BladeRotation : MonoBehaviour
+{
+	// Each seconds rotationSpeed/360 turns will be done
+	public float rotationSpeed = 20;
+
+	void Update(){
+		float amountToTurnRotate = this.rotationSpeed * Time.deltaTime;
+		this.transform.Rotate(0, 0, amountToTurnRotate, Space.Self);
+
+	}
+}
