ec01458e15d55685bc892713120e1af2525c9dfc
dodolebg <49374440+dodolebg@users.noreply.github.com>
2019-06-20 15:07:07 +0200

    Adding serveur launch on host and fixes

---
+++ oscourge_unity/Assets/Scripts/Server/serverLauncher.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using System;
+
+public class serverLauncher : MonoBehaviour
+{
+   public void Launch()
+    {
+        Process foo = new Process();
+        foo.StartInfo.FileName = Environment.CurrentDirectory + @"\Assets\Scripts\Server\launch.bat";
+        foo.StartInfo.Arguments = "1234";
+        foo.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+        foo.Start();
+        UnityEngine.Debug.Log("Server Launched");
+    }
+
+  
+}
